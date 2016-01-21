611924ff3638daf25dbfee27edaf95a8b4e11724
vsctiago <vsctiago@yahoo.com>
2016-01-21 17:12:27 +0000

    Added caminho and hora.

--- src/ED2015/Caminho.java
+++ src/ED2015/Caminho.java
@@ -7,9 +7,57 @@ package ED2015;
  */
 
 /**
- *
+ * Classe que identifica um caminho ou aresta com peso.
  * @author Utilizador
  */
 public class Caminho {
+    
+    private double precoKm;
+    private Hora[] horarios;
+    private double distancia;
+    private int duracao;
+
+    public Caminho(double precoKm, Hora[] horarios, double distancia, int duracao) {
+        this.precoKm = precoKm;
+        this.horarios = horarios;
+        this.distancia = distancia;
+        this.duracao = duracao;
+    }
+
+    public double getPrecoKm() {
+        return precoKm;
+    }
+
+    public Hora[] getHorarios() {
+        return horarios;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setPrecoKm(double precoKm) {
+        this.precoKm = precoKm;
+    }
+
+    public void setHorarios(Hora[] horarios) {
+        this.horarios = horarios;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+    
+    
+    
+    
     
 }
